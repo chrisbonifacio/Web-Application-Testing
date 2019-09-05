5483b8db11f61ecb9facfe30d346c5fc44ebf1b4
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-05 18:11:05 -0400

    add onClick to Hit button

--- baseball-app/src/components/Dashboard.jsx
+++ baseball-app/src/components/Dashboard.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import { FaBaseballBall } from "react-icons/fa";
+import { FaBaseballBall, FaWindowClose, FaCheck } from "react-icons/fa";
 
 export default class Dashboard extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <div className="Dashboard">
-        <FaBaseballBall></FaBaseballBall>
-        <span></span>
-        <span></span>
+        <button onClick={this.props.hitHandler}>Hit</button>
+        <button>Strike</button>
+        <button>Foul </button>
       </div>
     );
   }
