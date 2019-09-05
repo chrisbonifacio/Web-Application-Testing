b5b234549f1a71216c8cb67403ecb002792b802c
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-05 18:11:19 -0400

    display score from props

--- baseball-app/src/components/Display.jsx
+++ baseball-app/src/components/Display.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 
 export default class Display extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="Display">
         <h2>{this.props.score}</h2>
-        <h3>{this.props.balls}</h3>
-        <h3>{this.props.strikes}</h3>
       </div>
     );
   }
