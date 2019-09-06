c7b87395fb5a7511e01ce88835b661858ffc1d44
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-05 21:46:30 -0400

    add Fouls counter to display

--- baseball-app/src/components/Display.jsx
+++ baseball-app/src/components/Display.jsx
@@ -4,8 +4,9 @@ export default class Display extends React.Component {
   render() {
     return (
       <div className="Display">
-        <h2>Balls: {this.props.score}</h2>
+        <h2>Balls: {this.props.balls}</h2>
         <h2>Strikes: {this.props.strikes}</h2>
+        <h2>Fouls: {this.props.fouls}</h2>
       </div>
     );
   }
