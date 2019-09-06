83b9ce16aef7ff00646aa71a7e17c2aec978eef8
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-06 02:13:49 -0400

    add data-testid attributes to buttons

--- baseball-app/src/components/Dashboard.jsx
+++ baseball-app/src/components/Dashboard.jsx
@@ -5,10 +5,18 @@ export default class Dashboard extends React.Component {
   render() {
     return (
       <div className="Dashboard">
-        <button onClick={this.props.ballsHandler}>Ball</button>
-        <button onClick={this.props.hitsHandler}>Hit</button>
-        <button onClick={this.props.strikesHandler}>Strike</button>
-        <button onClick={this.props.foulHandler}>Foul</button>
+        <button data-testid="ball-button" onClick={this.props.ballsHandler}>
+          Ball
+        </button>
+        <button data-testid="hit-button" onClick={this.props.hitsHandler}>
+          Hit
+        </button>
+        <button data-testid="strike-button" onClick={this.props.strikesHandler}>
+          Strike
+        </button>
+        <button data-testid="foul-button" onClick={this.props.foulHandler}>
+          Foul
+        </button>
       </div>
     );
   }
