6c2a08df7da33922d734679617808177520df71d
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-06 02:15:01 -0400

    add data-testid attrbutes to elements displaying the values of balls, strikes, fouls

--- baseball-app/src/components/Display.jsx
+++ baseball-app/src/components/Display.jsx
@@ -5,9 +5,9 @@ export default class Display extends React.Component {
     return (
       <div className="Display">
         <div className="stats">
-          <h2>Balls: {this.props.balls}</h2>
-          <h2>Strikes: {this.props.strikes}</h2>
-          <h2>Fouls: {this.props.fouls}</h2>
+          <h2 data-testid="ball-value">Balls: {this.props.balls}</h2>
+          <h2 data-testid="strike-value">Strikes: {this.props.strikes}</h2>
+          <h2 data-testid="foul-value">Fouls: {this.props.fouls}</h2>
         </div>
         <div className="notifications">
           {this.props.strikes >= 3 && <p>Strike 3, You're OUT!</p>}
