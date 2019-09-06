857e8decc74dd1799bc8a7aafa7160c6418c4b13
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-05 22:29:46 -0400

    add some notifications for striking out and walking to first base after 4 balls

--- baseball-app/src/components/Display.jsx
+++ baseball-app/src/components/Display.jsx
@@ -4,9 +4,15 @@ export default class Display extends React.Component {
   render() {
     return (
       <div className="Display">
-        <h2>Balls: {this.props.balls}</h2>
-        <h2>Strikes: {this.props.strikes}</h2>
-        <h2>Fouls: {this.props.fouls}</h2>
+        <div className="stats">
+          <h2>Balls: {this.props.balls}</h2>
+          <h2>Strikes: {this.props.strikes}</h2>
+          <h2>Fouls: {this.props.fouls}</h2>
+        </div>
+        <div className="notifications">
+          {this.props.strikes >= 3 && <p>Strike 3, You're OUT!</p>}
+          {this.props.balls >= 4 && <p>4 balls, walk to first base</p>}
+        </div>
       </div>
     );
   }
